abe17ebb671943d7a37906d34a53b09aa876effd
Shane Garrity <shanedgarrity@gmail.com>
2018-02-06 00:56:15 -0500

    dummy hello world server @ localhost:3000

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,29 @@
 // app.get('/', (req, res) => res.send('Hello World!'))
 //
 // app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-console.log('duuuh');
-
 // let express = require('express');
 
 import * as express from 'express';
+import * as path from 'path';
+import * as ctrl from 'r/controller'
+
+console.log('duuuh');
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set('port', process.env.PORT || 3000);
+app.use((req, res, next) => {
+  next();
+});
+
+/**
+ * Primary app routes.
+ */
+// app.get('/', ctrl.index);
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(app.get('port'), () => console.log('listening on', app.get('port')));
+
+module.exports = app;
