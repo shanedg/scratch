85c797ca3ccfb8738e7129973dc441057b83496d
Shane Garrity <shanedgarrity@gmail.com>
2018-02-06 21:45:02 -0500

    add view stub for index route,  html string

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-// const express = require('express')
-// const app = express()
-//
-// app.get('/', (req, res) => res.send('Hello World!'))
-//
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
-// let express = require('express');
-
 import * as express from 'express';
 import * as path from 'path';
 import * as ctrl from 'r/controller'
@@ -22,8 +14,7 @@ app.use((req, res, next) => {
 /**
  * Primary app routes.
  */
-// app.get('/', ctrl.index);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', ctrl.index);
 
 app.listen(app.get('port'), () => console.log('scratch listening on', app.get('port')));
 
